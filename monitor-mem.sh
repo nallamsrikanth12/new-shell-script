975b4ad9df9003c5c588fd3092321c3537005189
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-30 20:03:02 +0630

    installation

---
+++ monitor-mem.sh
@@ -0,0 +1,14 @@
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e "$G file exists $N"
+else
+    echo -e "directory is not found"
+fi
+
