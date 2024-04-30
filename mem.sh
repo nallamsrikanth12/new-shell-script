0a76f554d52eed725ebbeff18533edd60a4c7b17
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-30 23:38:47 +0630

    installation

--- mem.sh
+++ mem.sh
@@ -9,7 +9,7 @@ while  IFS= read -r line
 do
   TOTAL_MEMORY_USAGE=$(echo $line | free -m |awk '/Mem/{print $2}' )
   AVALIABLE_USAGE=$(echo $line | free -m |awk '/Mem/{print $3}')
-if [ $AVALIABLE_USAGE -ge $MEM_THRESHOLD]
+if [ $AVALIABLE_USAGE -ge $MEM_THRESHOLD ]
 then
     echo "$AVALIALE_USAGE is more than $MEM_THRESHOLD and total memory is $TOTAL_MEMORY_USAGE mb"
 
