bdc430a63b73efe0c3fe481cd91d1674b76874f5
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-30 21:09:26 +0630

    installation

--- monitor-mem.sh
+++ monitor-mem.sh
@@ -1,20 +1,8 @@
-SOURCE_DIRECTORY=/tmp/app-logs
+#!/bin/bash
 
+# Email settings
+EMAIL_ADDRESS="nallamsrikanth3@gmail.com"
 THRESHOLD_PERCENTAGE=90
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ -d $SOURCE_DIRECTORY ]
-then
-    echo -e "$G file exists $N"
-else
-    echo -e "directory is not found"
-fi
-
-FILES=$( find $SOURCE_DIRECTORY -name "*.log" -mtime +14 ) 
 
 # Function to display memory usage
 show_memory_usage() {
@@ -22,12 +10,24 @@ show_memory_usage() {
     free -h
 }
 
+# Function to send email alert
+send_email_alert(){
+    SUBJECT="Memory Usage Alert on myserver"
+    MESSAGE="Memory usage has exceeded ${THRESHOLD_PERCENTAGE}% threshold on myserver. Current memory usage:"
+    echo "$MESSAGE" | mail -s "$memoryusage" "$nallamsrikanth3@gmail.com"
+}
 
-while IFS= read -r line
-do 
-  echo " $line :deleting files"
-  rm -rf $line
-done <<< $FILES
+# Main loop to continuously monitor memory usage
+while true
+do
+    MEMORY_USAGE=$(free | awk '/Mem/{printf("%.2f"), $3/$2*100}')
+    
+    clear  # Clear the screen before displaying memory usage
+    show_memory_usage
 
+    if [ "$(echo "$MEMORY_USAGE >= $THRESHOLD_PERCENTAGE" | bc)" -eq 1 ]; then
+        send_email_alert
+    fi
 
-
+    sleep 300  # Wait for 5 minutes before checking again (300 seconds)
+done
