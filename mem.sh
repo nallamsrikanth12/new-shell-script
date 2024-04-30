d2fa5807a02bb6bf44384866426335fb7dfd9f51
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-30 23:25:31 +0630

    installation

---
+++ mem.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TOTAL_MEMORY_USAGE=$(free -m |awk '/Mem/{print $2}' )
+
+MEM_THRESHOLD=200
+
+
+while 
+do 
+  AVALIABLE_USAGE=$(free -m |awk '/Mem/{print $3}')
+  echo "system avabile memory is $AVALIABLE_USAGE mb"
+if [ $AVALIABLE_USAGE -ge $MEM_THRESHOLD]
+then
+    echo "$AVALIALE_USAGE is more than $MEM_THRESHOLD and total memory is $TOTAL_MEMORY_USAGE mb"
+
+    #sleep for 1 second
+    sleep 1
+done
