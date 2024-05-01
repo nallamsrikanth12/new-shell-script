367284d3827610c96eebbf33d6305dc3ac63a6b2
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-05-01 17:25:57 +0630

    installation

--- mem.sh
+++ mem.sh
@@ -10,10 +10,9 @@ do
   AVALIABLE_USAGE=$(echo $line | free -m |awk '/Mem/{print $3}')
 if [ $AVALIABLE_USAGE -ge $MEM_THRESHOLD ]
 then
-  MESSAGE+= "$AVALIALE_USAGE is more than $MEM_THRESHOLD and total memory is $TOTAL_MEMORY_USAGE mb \n"
+  echo "$AVALIALE_USAGE is more than $MEM_THRESHOLD and total memory is $TOTAL_MEMORY_USAGE mb \n"
 
 fi   
 
 done <<< $MEM
 
-echo -e "message :$MESSAGE"
